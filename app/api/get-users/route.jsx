45c5bb217f3290d1d4c4214182b7bb7fd58828b6
BrianKellyCS <bkelly8946@gmail.com>
2024-05-15 15:12:10 -0700

    data cache fix

--- app/api/get-users/route.jsx
+++ app/api/get-users/route.jsx
@@ -1,15 +1,19 @@
 // api/get-users/route.jsx
 import { sql } from "@vercel/postgres";
 
+// Force Next.js to render this route dynamically
+export const dynamic = 'force-dynamic';
+
 // Handle GET requests
 export async function GET(request) {
   try {
     const { rows } = await sql`SELECT * FROM Users`;
-    console.log(rows);
+    //console.log(rows);
     return new Response(JSON.stringify(rows), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-store', // Ensure no caching on response
       },
     });
   } catch (error) {
@@ -17,6 +21,7 @@ export async function GET(request) {
       status: 500,
       headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-store', // Ensure no caching on error response
       },
     });
   }
